a78f2313298511ec56c127b3e5815f7f167ed201
Takahashi Shuhei <shuhei.takahashi@brainpad.co.jp>
2021-01-08 19:30:35 +0900

    initial commit

---
+++ src/devtools/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>This Is Devtools! {{ now }} </h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  computed: {
+    now: () => {
+      const hoge: string = 'abc';
+      return new Date() + hoge;
+    }
+  }
+}
+</script>
---
+++ src/devtools/main.ts
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import App from '@/devtools/App';
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
